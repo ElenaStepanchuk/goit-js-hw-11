5dc2c5fb47716f8a00c92d8bb5f7aad0bdfbcc4e
ElenaStepanchuk <podolyak365@gmail.com>
2021-11-20 21:00:38 +0200

    HTML разметка

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 import './sass/main.scss';
+
+import axios from "axios";
+import Notiflix from 'notiflix';
+import SimpleLightbox from "simplelightbox";
+
+const form = document.querySelector('.search-form');
+const input = document.querySelector('input[type="text"]');
+const button = document.querySelector('button[type="submit"]');
+
+console.log(form);
+console.log(input);
+console.log(button);
+
+form.addEventListener('submit', handleSubmitOnForm)
+
+function handleSubmitOnForm() {
+    
+}
