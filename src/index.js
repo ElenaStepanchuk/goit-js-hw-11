735b5915af075da7ea618012264880c8b63a98ec
ElenaStepanchuk <podolyak365@gmail.com>
2021-11-22 20:45:54 +0200

    установка библиотеки Lozad.js

--- src/index.js
+++ src/index.js
@@ -2,52 +2,66 @@ import './sass/main.scss';
 
 import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
-import { FetchToIP } from './js/fetch-photos';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import FetchToIP from './js/fetch-photos';
 import * as bootstrap from 'bootstrap';
-
+import lozad from 'lozad';
 
 const form = document.querySelector('.search-form');
-const input = document.querySelector('input[type="text"]');
-const button = document.querySelector('button[type="submit"]');
+// const input = document.querySelector('input[type="text"]');
+// const button = document.querySelector('button[type="submit"]');
 const gallery = document.querySelector('.gallery');
-const loadMoreButton = document.querySelector('.load-more'),
+// const loadMoreButton = document.querySelector('.load-more');
 
-fetchToIP = new FetchToIP();
+const fetchToIP = new FetchToIP();
 
 form.addEventListener('submit', handleSubmitOnForm);
-loadMoreButton.addEventListener('click', handleLoadMore);
+  const lightbox = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+    nav: true,
+    close: true,
+  });
+
+
+let totalHits = 0;
 
 function handleSubmitOnForm(event) {
-    event.preventDefault(event);
-    gallery.innerHTML = '';
-    fetchToIP.fetch = event.currentTarget.elements.searchQuery.value;
-    fetchToIP.resetPage();
+  event.preventDefault(event);
+  fetchToIP.request = event.currentTarget.elements.searchQuery.value.trim();
+  fetchToIP.resetPage();
 
+  if (fetchToIP.request === '') {
+    return Notiflix.Notify.warning(`Oops! You need to enter some value`);
+  }
 
-fetchToIP.getFetchPhotos()
-.then(response => {
-      Notiflix.Notify.success(`Hooray! We found ${response.totalHits} images.`);
-    checkAmountOfHits();
-      return response;
-})
-    .then(createGalleryItems)
-    .then(() => {
-      let gallery = new SimpleLightbox('.gallery a');
-      
-      return gallery;
-    })
-    form.reset();
- 
+  fetchToIP.getFetchPhotos()
+    .then(hits => {
+      handleClearPhotoConteiner();
+      handleAddCreateGallery(hits);
+   });
+   
 }
 
+function handleAddCreateGallery(hits) {
+  totalHits += hits.length;
+  if (hits.length === 0) {
+    return Notiflix.Notify.failure(
+      `Sorry, there are no images matching your search query. Please try again.`,
+    );
+  } 
+  Notiflix.Notify.success(`Hooray! We found ${totalHits} images for you`);
+  createGalleryItems({ hits });
+}
 
 function createGalleryItems({hits}) {
     
-    const galleryCreate = hits.map(hit => { 
-            return `<a class="gallery__item" href="${hit.largeImageURL}">
+ let photoArray = hits.map(hit => {
+    return `<a class="gallery__item" href="${hit.largeImageURL}">
     <div class="photo-card">
-<img src="${hit.webformatURL}" alt="${hit.tags}" class="gallery-img"loading="lazy" />
+<img src="${hit.webformatURL}" alt="${hit.tags}" class="gallery-img lozad" loading="lazy" />
   <div class="info">
     <p class="info-item">
       <b class="info-description">Likes</b>
@@ -62,52 +76,26 @@ function createGalleryItems({hits}) {
     <span class="info-number">${hit.comments}</span>
     </p>
     <p class="info-item">
-      b class="info-description">Downloads</b>
+      <b class="info-description">Downloads</b>
     <span class="info-number">${hit.downloads}</span>
     </p>
    </div>
   </div>
   </a>`
-    }).join('');
-    gallery.insertAdjacentHTML('beforeend', galleryCreate);
+ }).join('');
+  gallery.insertAdjacentHTML('beforeend', photoArray);
+  lightbox.refresh();
 }
 
-    
-   
-function handleLoadMore(response) {
-
-  fetchToIP.getFetchPhotos()
-    .then(response => {
-        checkAmountOfHits();
-      return response;
-    }).then(createGalleryItems)
-    .then(() => {
-      let gallery = new SimpleLightbox('.gallery a');
-   
-      return gallery;
-    }).then(() => {
-      const { height: cardHeight } = document
-      .querySelector('.gallery')
-      .firstElementChild.getBoundingClientRect();
-
-      window.scrollBy({
-        top: cardHeight * 2,
-        behavior: 'smooth',
-      });
-    });
-  
+ function handleClearPhotoConteiner() {
+  totalHits = 0;
+  gallery.innerHTML = '';
 }
 
-
-function checkAmountOfHits() {
-  console.log(fetchToIP.totalHits/fetchToIP.hits.length)
-  
-     if ((response.totalHits/response.hits.length)<=1) {
-     loadMoreButton.classList.add('disabled');
-      }
-
-        loadMoreButton.classList.remove('disabled');
-  }
-
-
+const observer = lozad('.lozad', {
+    rootMargin: '10px 0px', // syntax similar to that of CSS Margin
+    threshold: 0.1, // ratio of element convergence
+    enableAutoReload: true // it will reload the new image when validating attributes changes
+});
+observer.observe();
 
--- src/js/fetch-photos.js
+++ src/js/fetch-photos.js
@@ -1,62 +1,41 @@
 import axios from "axios";
+
 const API_KEY = '24384103-764a450d164e25b7c6f60e4ce';
 const URL = 'https://pixabay.com/api/';
+const OPTIONS = 'image_type=photo&orientation=horizontal&safesearch=true';
 
 
-export class FetchToIP {
+export default class FetchToIP {
     constructor() {
-        this.fetch = "";
+        this.request = ``;
         this.page = 1;
+        this.per_page = 40;
+        this.totalHits = 1;
     }
 
 
     async getFetchPhotos() {
         
-        const params = new URLSearchParams( {
-            key: API_KEY,
-            q: '${this.fetch}',
-            image_type: 'photo',
-            orientation: 'horizontal',
-            safesearch: 'true',
-            page: `${this.page}`,
-            per_page: 40,
-        })
-
-        const response = await axios.get(`${URL}?${params}`)
+        
+        const url = `${URL}?key=${API_KEY}&q=${this.request}&${OPTIONS}&page=${this.page}&per_page=${this.per_page}`;
         try {
-            if (response.data.hits.length === 0) {
-                return Notify.failure("Sorry, there are no images matching your search query. Please try again.");
-            }
+            const response = await axios.get(url);
             this.page += 1;
-            return await response.data;
+            return await response.data.hits;
+            
         }
         catch (error) {
-            console.log(error.message)
+            console.log(error)
         }
     }
     resetPage() {
         this.page = 1;
     }
+    get reply() {
+    return this.request;
+  }
+
+  set reply(newReply) {
+    this.request = newReply;
+  }
 }
-// collections: 8037
-// comments: 396
-// downloads: 366937
-// id: 736877
-// imageHeight: 1282
-// imageSize: 97150
-// imageWidth: 1920
-// largeImageURL: "https://pixabay.com/get/g116aa7e24f203aeb5eae264a314b57123404d2c61ae1819c8e2182e8dfa93d2a1b0e71519fcdb0d0536b42382d4de6910dc1bc98aea188720d38241c02916574_1280.jpg"
-// likes: 2337
-// pageURL: "https://pixabay.com/photos/tree-cat-silhouette-moon-full-moon-736877/"
-// previewHeight: 100
-// previewURL: "https://cdn.pixabay.com/photo/2015/04/23/21/59/tree-736877_150.jpg"
-// previewWidth: 150
-// tags: "tree, cat, silhouette"
-// type: "photo"
-// user: "Bessi"
-// userImageURL: "https://cdn.pixabay.com/user/2019/04/11/22-45-05-994_250x250.jpg"
-// user_id: 909086
-// views: 792727
-// webformatHeight: 427
-// webformatURL: "https://pixabay.com/get/gd900567c55d5c33743ea8f0320c26213460a17cf6f31483b55c1cdc8df0f55a72bfedb4e366ecdbbc34fe6da30e8c874_640.jpg"
-// webformatWidth: 640
